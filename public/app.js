19697651723f70f9131cd2436c4bc45dfc6c400e
Alex Alvarado <alexjalvarado66@gmail.com>
2020-02-20 21:41:28 -0500

    changes to html img, submit search on enter

--- public/app.js
+++ public/app.js
@@ -7,6 +7,18 @@ $(function() {
   });
 
   $("#searchSubmit").click(function() {
+    returnData();
+  });
+
+  $("#search").on({
+    keydown: function(event) {
+      if (event.which === 13) {
+        returnData();
+      }
+    }
+  });
+
+  function returnData() {
     $.post(
       "/params",
       {
@@ -17,16 +29,18 @@ $(function() {
       function(data, status) {
         console.log(data);
 
+        $("#resultName")
+          .text(data.name)
+          .before("<img src=''></img>");
+
         $("img")
           .attr("src", data.image_url)
           .attr("height", "20%")
           .attr("width", "20%");
 
-        $("#resultName").text(data.name);
-
         $("#resultAddr1").text(data.location.display_address[0]);
         $("#resultAddr2").text(data.location.display_address[1]);
       }
     );
-  });
+  }
 });
